4afbc32ab92d5d571ebee52ce21ec8bc3d3657c8
Fachrul Dwi Aprilian <99337701+aprlfch@users.noreply.github.com>
2026-08-02 01:17:14 +0800

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import USER from "./images/user.png";
 import LOGO from "./images/SIMKO.png";
 import LOGOKOPERASI from "./images/logo1.png";
+import KOPERASI from "./images/KOPERASI.png";
 
-export { USER, LOGO, LOGOKOPERASI };
+export { USER, LOGO, LOGOKOPERASI, KOPERASI };
